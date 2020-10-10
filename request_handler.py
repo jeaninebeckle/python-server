c5733d5a1aa96f1d8c1ca1666763133b0f854362
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-10-10 10:15:00 -0500

    can post animals, customers, employees, locations

--- request_handler.py
+++ request_handler.py
@@ -3,8 +3,8 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal
 from locations import get_all_locations, get_single_location, create_location
-from employees import get_all_employees, get_single_employee
-from customers import get_all_customers, get_single_customer
+from employees import get_all_employees, get_single_employee, create_employee
+from customers import get_all_customers, get_single_customer, create_customer
 
 
 # Here's a class. It inherits from another class.
@@ -97,6 +97,17 @@ class HandleRequests(BaseHTTPRequestHandler):
             new_location = None
             new_location = create_location(post_body)
             self.wfile.write(f"{new_location}".encode())
+
+        elif resource == "employees":
+            new_employee = None
+            new_employee = create_employee(post_body)
+            self.wfile.write(f"{new_employee}".encode())
+
+        elif resource == "customers":
+            new_customer = None
+            new_customer = create_customer(post_body)
+            self.wfile.write(f"{new_customer}".encode())
+
             
         # Encode the new animal and send in response
         # self.wfile.write(f"{new_animal}".encode())
