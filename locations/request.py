3d49486174831a94ecc326d73a016df82fc58c93
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-10-13 19:58:27 -0500

    enable sql

--- locations/request.py
+++ locations/request.py
@@ -1,13 +1,62 @@
+import sqlite3
+import json
+
 from models.location import Location
 
 LOCATIONS = [
-    Location(1, "Nashville North", "8422 Johnson Pike"),
-    Location(2, 'Nashville South', "209 Emory Drive"),
-    Location(3, 'Nashville West', "100 Charlotte Pike")
+    # Location(1, "Nashville North", "8422 Johnson Pike"),
+    # Location(2, 'Nashville South', "209 Emory Drive"),
+    # Location(3, 'Nashville West', "100 Charlotte Pike")
 ]
 
 def get_all_locations():
-    return LOCATIONS
+    with sqlite3.connect("./kennel.db") as conn:
+
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.address
+        FROM location a
+        """)
+
+        locations = []
+
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+
+            location = Location(row['id'], row['name'], row['address'])
+
+            locations.append(location.__dict__)
+
+    return json.dumps(locations)
+
+    
+def get_single_location(id):
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.address
+        FROM location a
+        WHERE a.id = ?
+        """, ( id, ))
+
+        data = db_cursor.fetchone()
+
+        location = Location(data['id'], data['name'], data['address'])
+
+        return json.dumps(location.__dict__)
 
 def create_location(location):
     max_id = LOCATIONS[-1].id
@@ -22,17 +71,17 @@ def create_location(location):
     return location
 
     
-def get_single_location(id):
+# def get_single_location(id):
    
-    requested_location = None
+#     requested_location = None
 
-    for location in LOCATIONS:
-        # Dictionaries in Python use [] notation to find a key
-        # instead of the dot notation that JavaScript used.
-        if location.id == id:
-            requested_location = location
+#     for location in LOCATIONS:
+#         # Dictionaries in Python use [] notation to find a key
+#         # instead of the dot notation that JavaScript used.
+#         if location.id == id:
+#             requested_location = location
 
-    return requested_location
+#     return requested_location
 
 def delete_location(id):
     location_index = -1
--- models/employee.py
+++ models/employee.py
@@ -3,15 +3,13 @@ class Employee():
     # Class initializer. It has 5 custom parameters, with the
     # special `self` parameter that every method on a class
     # needs as the first parameter.
-    def __init__(self, id, name, location, manager, full_time, hourly_rate):
+    def __init__(self, id, name, address, location_id):
         self.id = id
         self.name = name
-        self.location = location
-        self.manager = manager
-        self.full_time = full_time
-        self.hourly_rate = hourly_rate
+        self.address = address
+        self.location_id = location_id
 
     def __repr__(self):
-        return f"{self.name} is an employee and makes ${self.hourly_rate} an hour."
+        return f"{self.name} is an employee."
 
-employee = Employee(1, 'Sally', 2, True, True, 20)
+# employee = Employee(1, 'Sally', 2, True, True, 20)
