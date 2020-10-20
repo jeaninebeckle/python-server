6e016eafe8c46219254c4a59aca4465321809c67
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-10-19 19:55:33 -0500

    edit functionality on animals working

--- animals/request.py
+++ animals/request.py
@@ -97,30 +97,6 @@ def create_animal(animal):
     return animal
 
 
-def delete_animal(id):
-    # Initial -1 value for animal index, in case one isn't found
-    animal_index = -1
-
-    # Iterate the ANIMALS list, but use enumerate() so that you
-    # can access the index value of each item
-    for index, animal in enumerate(ANIMALS):
-        if animal.id == id:
-            # Found the animal. Store the current index.
-            animal_index = index
-
-    # If the animal was found, use pop(int) to remove it from list
-    if animal_index >= 0:
-        ANIMALS.pop(animal_index)
-
-def update_animal(id, updated_animal):
-    # Iterate the ANIMALS list, but use enumerate() so that
-    # you can access the index value of each item.
-    for index, animal in enumerate(ANIMALS):
-        if animal.id == id:
-            # Found the animal. Update the value.
-            ANIMALS[index] = Animal(updated_animal['id'], updated_animal['name'], updated_animal['status'], updated_animal['breed'], updated_animal['customer_id'], updated_animal['location_id'])
-            break
-
 def get_animals_by_loc_id(location_id):
 
     with sqlite3.connect("./kennel.db") as conn:
@@ -185,3 +161,31 @@ def delete_animal(id):
         DELETE FROM animal
         WHERE id = ?
         """, (id, ))
+
+def update_animal(id, new_animal):
+    with sqlite3.connect("./kennel.db") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        UPDATE Animal
+            SET
+                name = ?,
+                breed = ?,
+                status = ?,
+                location_id = ?,
+                customer_id = ?
+        WHERE id = ?
+        """, (new_animal['name'], new_animal['breed'],
+              new_animal['status'], new_animal['location_id'],
+              new_animal['customer_id'], id, ))
+
+        # Were any rows affected?
+        # Did the client send an `id` that exists?
+        rows_affected = db_cursor.rowcount
+
+    if rows_affected == 0:
+        # Forces 404 response by main module
+        return False
+    else:
+        # Forces 204 response by main module
+        return True
